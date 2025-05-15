5be6befc565680a255d010f0c54dafa24d8ef006
Kian731 <140600174+Kian731@users.noreply.github.com>
2025-05-15 14:57:41 +0000

    main

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 function info() {    
     const para = document.createElement("p");
     para.innerHTML = "更多資訊在這裡";
-    document.main.appendChild(para);
+    document.querySelector("main").appendChild(para);
 }
 
 var date = new Date();
