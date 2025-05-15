a77b0f36bf87b80116d5851cfb174c9f831cbd3f
Kian731 <140600174+Kian731@users.noreply.github.com>
2025-05-15 14:53:43 +0000

    js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 
-function info(){    
+
+function info() {    
     const para = document.createElement("p");
     para.innerHTML = "更多資訊在這裡";
-    document.getElementById(more).appendChild(para);
-};
+    document.getElementById("more").appendChild(para);
+}
 
-var date = new Date()
-document.getElementById(today).innerHTML=date;
-
+var date = new Date();
+document.getElementById("today").innerHTML = date.toLocaleDateString(); // 美化日期格式
